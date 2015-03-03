8a3f7631f69660e4d9628b3ccb9319e2054f1ac6
noureldien <nreldien@yahoo.com>
2015-03-03 01:02:50 +0000

    Commit

--- algo1.m
+++ algo1.m
@@ -10,16 +10,22 @@ load('Data\t_train.mat');
 load('Data\t_test.mat');
 load('Data\t_truth.mat');
 
-R = [5,3,0,1;
-    4,0,0,1;
-    1,1,0,5;
-    1,0,0,4;
-    0,1,5,4];
-
+% just train your model
+% on a small portion of the t_test
 R = t_test;
-R = R(1:100,:);
-R = R(:,1:20);
+R = R(1:50,:);
+R = R(:,1:6);
 
+% truth 
+T = t_truth;
+T = T(1:50,:);
+T = T(:,1:6);
+
+% because this model is built to work
+% on ranges from 1 to N (0 is missing data)
+% and the training data we have is a bit diffirent
+% (ranges of ratings form -10 to 10, where 99 is a missing data)
+% so, these 2 lines is to fix that
 R(R == 99) = -11;
 R = R + 11;
 
@@ -27,22 +33,38 @@ R = R + 11;
 [N, M] = size(R);
 
 % size of the latent features
-K = 10;
+K = 8;
 
 % parameters of the alogirthm
+%alpha : the learning rate
+%beta  : the regularization parameter
 steps=5000;
-alpha=0.0002;
-beta=0.02;
+alpha=0.001;
+beta=0.2;
 
 % factorize the rating matrix
 P = rand(N,K);
 Q = rand(M,K);
 
-[nP, nQ] = matrixFactorization(R, P, Q, K, steps, alpha, beta);
+[nP, nQ] = naiveMF(R, P, Q, K, steps, alpha, beta);
 nR = nP*nQ';
+
+% shift back the data
+nR = nR - 11;
 
 % check against truth
 %nR vs. t_truth
+confusion = abs(nR - T)';
+rmse = reshape(confusion,[size(confusion,1)*size(confusion,2),1]);
+rmse = sqrt(mean((rmse).^2));
+disp(strcat('Final Error: ', num2str(rmse)));
+
+% plot the result
+figure(1); clf;
+imshow(confusion, 'InitialMagnification', 1000);
+title(strcat('RMSE: ', num2str(rmse)));
+colormap(jet);
+colorbar;
 
 
 
--- testingLab.m
+++ testingLab.m
@@ -1,7 +1,4 @@
 clc;
 
-X = rand(40);
 
-Y = randi([1,40],10, 1);
 
-missingRatio
