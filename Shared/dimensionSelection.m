bbd16c87d4ce8e257730d9d313957eeb6a6609dc
noureldien <nreldien@yahoo.com>
2015-03-06 18:43:19 +0000

    Commit

--- Shared/dimensionSelection.m
+++ Shared/dimensionSelection.m
@@ -1,48 +1,50 @@
-function [ clusters, indeces ] = observSelection( data, errorThreshold, varThreshold, missings )
+function [ clusters, indeces ] = dimensionSelection( data, errorThreshold, varThreshold, missings )
 
-% group users/observations into clusters, each cluster has a group
-% of similar users. a cluster called user pattern
-% the rmse between any pair of features
+% group features into clusters, each cluster has a group
+% of similar features, where the rmse between any pair of features
 % in one cluster is less than or equal to the given threshold
 %
 % input:
 %         data      : M*N, where M observations, N features
-%    errorThreshold : over this threshold, the 2 users are not considered correlated
+%    errorThreshold : over this threshold, the 2 features are not considered correlated
 %      varThreshold : a feature with variance over it is considered noisy and not picked up in the cluster
 %
 % output:
-%          clusters : list of users features (user patterns)
+%          clusters : list of clustered features
 
 
 K = length(missings);
 [M,N] = size(data);
 
+disp('Start Calculating Correlation Errors');
+
 % get matrix that represent the pair-wise rmse
 % between the features
-errors = zeros(M,M);
-for i=1:M
-    for j=i:M
+errors = zeros(N,N);
+for i=1:N
+    disp(i);
+    for j=i:N
         if (i==j)
             error = 0;
         else            
-            % check if no missing data, then get all features
-            % of the user. if missing data, get only
-            % the common features with no missing data
+            % check if no missing data, then get all observations
+            % of the features. if missing data, get only
+            % the common observations with no missing data
             if (K == 0)
-                u1 = data(i,:);
-                u2 = data(j,:);
+                f1 = data(:,i);
+                f2 = data(:,j);
             else
                 idx1 = [];
                 idx2 = [];
                 for k=1:K
-                    idx1 = [idx1; find(data(i,:) ~= missings(k))];
-                    idx2 = [idx2; find(data(j,:) ~= missings(k))];
+                    idx1 = [idx1; find(data(:,i) ~= missings(k))];
+                    idx2 = [idx2; find(data(:,j) ~= missings(k))];
                 end
                 idx = intersect(idx1, idx2);
-                u1 = data(i,idx);
-                u2 = data(j,idx);
+                f1 = data(idx,i);
+                f2 = data(idx,j);
             end
-            error = sqrt(mean((u1-u2).^2));
+            error = sqrt(mean((f1-f2).^2));
         end        
         errors(i,j) = error;
         errors(j,i) = error;
@@ -64,26 +66,28 @@ clusters = {};
 % also save your choices in a matrix
 indeces = [];
 
-% loop on the coeffs of the users
-for i=1:M
-    
+disp('Start Clustering');
+
+% loop on the coeffs of the features
+for i=1:N
+    disp(i);
     isCorrelated = false;
     
-    % loop on the clusters of users
+    % loop on the clusters of features
     for j=1:size(clusters,1)
         
-        % loop on each user in the current cluster
+        % loop each the features in the current cluster
         for k=1:size(clusters,2)
             
-            % the id of the user in the current cluster
-            clusteredUserID = clusters{j,k};
+            % the id of the feature in the current cluster
+            clusteredFeatureID = clusters{j,k};
             
-            % check the error between the current user
-            % and the clustered user is below threshold
-            isCorrelated = errors(clusteredUserID,i) <= errorThreshold;
+            % check the error between the current feature
+            % and the clustered feature is below threshold
+            isCorrelated = errors(clusteredFeatureID,i) <= errorThreshold;
             
-            % if only one clustered user is not correlated
-            % with the current user, then the current user
+            % if only one clustered feature is not correlated
+            % with the current feature, then the current feature
             % is not correlated with all the cluster
             if (~isCorrelated)
                 break;
@@ -98,13 +102,13 @@ for i=1:M
     end
     
     % now, if there is correlation
-    % then add the user to the cluster
+    % then add the feature to the cluster
     if (isCorrelated)
         clusters{j,:} = [clusters{j,:} i];
         indeces = [indeces; [i, j]];
     else
         % if no correlation is found, then check if
-        % the user is noisy or not, by checking it's variance
+        % the feature is noisy or not, by checking it's variance
         % if it is noisy, then add it in a new cluster
         if (vars(i) <= varThreshold)
             clusters = [clusters; i];
@@ -115,3 +119,4 @@ for i=1:M
 end
 
 end
+
--- testingLab.m
+++ testingLab.m
@@ -9,47 +9,38 @@ load('Data\t_train.mat');
 [~,N] = size(t_train);
 
 % do feature reduction/selection
-varianceThreshold = 33;
-errorThreshold = 5.6;
-[fClusters, fIdx] = featureSelection(t_train, errorThreshold, varianceThreshold, [99]);
+varianceThr = 33;
+errorThr = 5.6;
+[fClusters, fIdx] = dimensionSelection(t_train, errorThr, varianceThr, [99]);
 
 % train-Feature-Reducted
-t_trainFRed = featureReduction(t_train, fClusters, [99]);
+t_trainFRed = dimensionReduction(t_train, fClusters, [99]);
 
 % reduce the test set using the same cluster
-t_testFRed = featureReduction(t_test, fClusters, [99]);
+t_testFRed = dimensionReduction(t_test, fClusters, [99]);
 
 disp('Done Feature Reduction');
 
 % how many reducted features we have
 nRed = length(fClusters);
 
-% how many patterns of bservations we want to have
-[M, ~] = size(t_train);
-K = int16(M/10);
+% cluster the users to K cluster
+varianceThr = 33;
+errorThr = 5.6;
+[uClusters, uIdx] = dimensionSelection(t_trainFRed', errorThr, varianceThr, [99]);
 
-% sanitize the train before passing it to k-means
-% this is to hide the 99, 55
-% train-feature-reducted-and-sanitized
-t_trainFRedSa = t_trainFRed;
-t_trainFRedSa(t_trainFRedSa==99) = 0.00099;
-
-% cluster the users to K cluster, don't use k-means
-% as it will take forever
-%[t_trainKmIdx, t_trainKmCenters] = kmeans(t_trainFRedSa, K);
-%t_trainKmCenters = t_trainKmCenters';
-[uClusters, uIdx] = featureSelection(t_train', errorThreshold, varianceThreshold, [99]);
+tic
+% train-Feature-Reducted-user-reducted
+t_trainFRedURed = dimensionReduction(t_trainFRed', fClusters, [99]);
+toc
 
 disp('Done User Clustering');
 return;
 
-% restore the 99
-t_trainKmCenters(t_trainKmCenters==0.00099) = 99;
-
 % after clustering the users into patterns of users
 % match each user in the training set with the pattern
 % then predict the missing values
-estmTest =  observPrediction(t_trainKmCenters, t_testFRed, [99], [99]);
+estmTest = observPrediction(t_trainFRedURed, t_testFRed, [99], [99]);
 
 disp('Done Predicting the missing');
 
