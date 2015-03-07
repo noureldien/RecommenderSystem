d3940fa2b38b9947af52eed433bfd067f3a2ef49
noureldien <nreldien@yahoo.com>
2015-03-07 13:40:44 +0000

    Commit

--- Experiment/exp11.m
+++ Experiment/exp11.m
@@ -66,6 +66,17 @@ saveResult(test, estmTestRecov, 19);
 
 disp('Done Saving Results');
 
+% one step is using the estimated test, place 55 in it
+% and estimate 55
+test55 = estmTestRecov;
+test55(test==55) = 55;
+estmTest55 = dataCompletion(test55, [55]);
+
+% save the results
+saveResult(test, estmTestRecov, 20);
+
+
+
 
 
 
--- testingLab.m
+++ testingLab.m
@@ -1,9 +1,8 @@
-% get the errors
-[~, rmseEstm, ~] = calcError(fourk, fourk55, estm55, [55]);
-disp(rmseEstm);
-% this was 4.02
+% one step is using the estimated test, place 55 in it
+% and estimate 55
+test55 = estmTestRecov;
+test55(test==55) = 55;
+estmTest55 = dataCompletion(test55, [55]);
 
-% get the errors
-[~, rmseEstm, ~] = calcError(fourk, fourk55, estm, [55]);
-disp(rmseEstm);
-% this was 3.99
+% save the results
+saveResult(test, estmTestRecov, 20);
